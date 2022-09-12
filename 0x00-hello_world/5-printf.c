3a72b42463a5be7f612c723cdb9995863ff0c6be
root <drtchidis450@gmail.com>
2022-09-12 04:03:55 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,17 @@
-#include <stdio.h>                                                                                                                              
-                                                                                                                                                
-/**                                                                                                                                             
-* main - print the string in the put function                                                                                                   
-*                                                                                                                                               
-* Description: using the main function                                                                                                          
-* this program prints "Programming is like building a multilingual puzzle                                                                       
-* Returns: 0                                                                                                                                    
-*/                                                                                                                                              
-int main(void)                                                                                                                                  
-{                                                                                                                                               
-        printf("with proper grammar, but the outcome is a piece of art,\n");                                                                    
-        return (0);                                                                                                                             
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* This programe prints "Programming is like building a multilingual puzzle
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
