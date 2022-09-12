82bf8ac2daf9e5cb4f2cab689b44b05b34cebe63
root <drtchidis450@gmail.com>
2022-09-12 04:50:11 -0700

    6-siz

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the function
- *
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: Always 0 (success)
- */
+* main - print the string in the function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 	char c;
