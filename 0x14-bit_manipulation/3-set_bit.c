c1add24cf6127c31a020549a1ef0e086f3e982d5
drtchidis450 <chidiebere.ojike@openpesa.co.uk>
2022-10-27 17:50:05 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	/* considering 64 bits */
-    if (index > 63)
-		return (-1);
+	   /* considering 64 bits */
+	if (index > 63)
+	       	return (-1);
 
 	*n = ((1UL << index) | *n);
 	return (1);
